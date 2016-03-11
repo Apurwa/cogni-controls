ca6ee0af77ec52ae3abe5d9087c7c3cd9b3eab6a
Apurwa Sarwajit <apurvsingh28@gmail.com>
2016-03-12 02:43:57 +0530

    setting up

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<ul class="nav nav-pills">
+
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+     
+    </ul>
+  </li>
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+     
+    </ul>
+  </li>
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+     
+    </ul>
+  </li>
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+     
+    </ul>
+  </li>
+  
+</ul>
+</body>
+</html>
