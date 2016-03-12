fa34e7eac752bddc9dce800f17a02a25c097de59
NIKHIL <krsoninikhil@gmail.com>
2016-03-12 08:40:03 +0530

    adds ids to input element

--- views/control2.html.php
+++ views/control2.html.php
@@ -1,4 +1,4 @@
-<div class="col-sm-12 c1">
+<div class="col-sm-12 c2">
 	<div class="login-control"><h2>Control-2: Allotments (Kit, ID and Room)</h2></div>
 	<div class="login-control"><span class="btn btn-success">Refresh</span></div>
 	<table class="table allotments">
@@ -11,15 +11,15 @@
 			<th></th>
 		</tr>
 		<tr>
-			<td><input type="text" class="form-control" placeholder="Cogni-ID" disabled></td>
-			<td><input type="checkbox" class="form-control" name="noc" value="1"></td>
-			<td><input type="checkbox" class="form-control" name="college_id" value="1"></td>
-			<td><input type="text" class="form-control" placeholder="Bhawan"></td>
-			<td><input type="text" class="form-control" placeholder="Room No."></td>
+			<td><input type="text" class="form-control" placeholder="Cogni-ID" id="cogni_id" disabled></td>
+			<td><input type="checkbox" class="form-control" name="kit" value="1" id="kit"></td>
+			<td><input type="checkbox" class="form-control" name="id" value="1" id="id"></td>
+			<td><input type="text" class="form-control" placeholder="Bhawan" id="bhawan"></td>
+			<td><input type="text" class="form-control" placeholder="Room No." id="room"></td>
 			<td></td>
 		</tr>
 	</table>
-	<div class="login-control"><span class="btn btn-success">Submit</span></div>
+	<div class="login-control"><span class="btn btn-success" id="summit">Submit</span></div>
 	<table class="table c1-details">
 		<tr>
 			<th>Cogni-ID</th>
